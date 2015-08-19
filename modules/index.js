2e7b0da55db9077c5e843d61d44d1b681bd5f16e
Matthias Le Brun <mlbli@me.com>
2015-08-19 19:51:22 +0200

    fixed dep

--- modules/index.js
+++ modules/index.js
@@ -1,14 +1,16 @@
 import autoprefix from "./autoprefix"
 import createStyleSheet from "./createStyleSheet"
 import {em, rem, percent, deg, vw, vh} from "./units"
+import resizeable from "./react/resizeable"
+import ResizeProvider from "./react/ResizeProvider"
 
-export default {
-  createStyleSheet,
-  em,
-  rem,
-  percent,
-  deg,
-  vw,
-  vh,
-  __VERSION__,
-}
+export { createStyleSheet as createStyleSheet }
+export { em as em }
+export { rem as rem }
+export { percent as percent }
+export { deg as deg }
+export { vw as vw }
+export { vh as vh }
+export { resizeable as resizeable }
+export { ResizeProvider as ResizeProvider }
+export { __VERSION__ as version }
--- modules/react/index.js
+++
@@ -1,7 +0,0 @@
-import resizeable from "./resizeable"
-import ResizeProvider from "./ResizeProvider"
-
-export default {
-  resizeable,
-  ResizeProvider,
-}
